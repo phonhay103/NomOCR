26f0129476323abfc3a3deaeed14ff4f1ddf5c5a
phonhay103 <phonhay103@gmail.com>
2023-07-19 17:25:00 +0700

    train model

--- metrics/calculate.py
+++ metrics/calculate.py
@@ -1,18 +1,17 @@
 import numpy as np
-from tqdm import tqdm
 import pandas as pd
 
 # Load lpips values
-with open('lpips_32_32.txt') as f:
+with open('ssim_16_16.txt') as f:
     lpips = [line.split('\t') for line in f.read().splitlines()]
 
-df = pd.DataFrame(lpips, columns=['filename', 'lpips'])
-df = df.astype({'lpips': np.float32})
+df = pd.DataFrame(lpips, columns=['filename', 'ssim'])
+df = df.astype({'ssim': np.float32})
 
 # Load gan labels
-with open('../NOM_CGGAN_train.txt') as f:
+with open('../NOM_CGGAN_errors_mean.txt') as f:
     filenames = [line.split('\t')[0] for line in f]
 
 # Get lpips
 df = df[df.filename.isin(filenames)]
-print(df.lpips.mean())
+print(df['ssim'].mean())
